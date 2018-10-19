86413b8c0f1d2bb97343f93024e71e7f89e65005
Mihail Parvanov <mihail.parvanov@telerik.com>
2018-10-19 16:09:20 +0300

    Add Calculator MVC project

---
+++ Calculator/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(AdditionViewModel model, string command)
+        {
+            if (command == "add")
+            {
+                model.Result = model.A + model.B;
+            }
+            if (command == "sub")
+            {
+                model.Result = model.A - model.B;
+            }
+            if (command == "mul")
+            {
+                model.Result = model.A * model.B;
+            }
+            if (command == "div")
+            {
+                model.Result = model.A / model.B;
+            }
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
